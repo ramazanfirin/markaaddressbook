04c7a23cf5543e1ade892f2e81c9f14f1a618ce5
ramazanfirin@gmail.com <ramazanfirin@gmail.com@87e2d7d0-462a-b2c6-711f-0641277de5cd>
2012-08-11 12:21:21 +0000

    v0.9.4

--- jAddressTest/config/import.sql
+++ jAddressTest/config/import.sql
@@ -1,4 +1,4 @@
 INSERT INTO `Authority` VALUES (1, 'Admin');
 INSERT INTO `Authority` VALUES (2, 'User');
-INSERT INTO `Users` VALUES (1, 'admin','YWRtaW4=','admin','admin','555122122',1);
-INSERT INTO `Users` VALUES (2, 'user','dXNlcg==','user','user','555122123',2);
+INSERT INTO `Users` VALUES (1, 'admin','YWRtaW4=','admin','admin','5551221229',1);
+INSERT INTO `Users` VALUES (2, 'user','dXNlcg==','user','user','5551221239',2);
