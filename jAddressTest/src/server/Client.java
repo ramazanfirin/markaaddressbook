0603ff991748cc37916797928371820e8d1773fc
ramazanfirin@gmail.com <ramazanfirin@gmail.com@87e2d7d0-462a-b2c6-711f-0641277de5cd>
2012-08-24 11:32:34 +0000

    v1.1.0-Client/server ayrımı yapıldı

---
+++ jAddressTest/src/server/Client.java
@@ -0,0 +1,26 @@
+package server;
+
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+
+public class Client {
+	  private Client() {
+	  }
+	 
+	  public static void main(String[] args) {
+	    //create WebService client proxy factory
+	    JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+	    //register WebService interface
+	    factory.setServiceClass(IHelloWorld.class);
+	    //set webservice publish address to factory.
+	    factory.setAddress("http://localhost:9000/HelloWorld");
+	    
+	    IHelloWorld iHelloWorld = (IHelloWorld) factory.create();
+	    System.out.println("invoke webservice...");
+	    System.out.println("message context is:" + iHelloWorld.loadAllDriver2());
+	    
+	    
+	    
+	    
+	    System.exit(0);
+	  }
+	}
---
+++ jAddressTest/src/server/Server.java
@@ -0,0 +1,27 @@
+package server;
+
+import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
+
+public class Server {
+	public Server() {
+	  IHelloWorld helloWorld = new HelloWorldImpl();
+	  //create WebService service factory
+	  JaxWsServerFactoryBean factory = new JaxWsServerFactoryBean();
+	  //register WebService interface
+	  factory.setServiceClass(IHelloWorld.class);
+	  //publish the interface
+	  factory.setAddress("http://localhost:9000/OtobusFihrist");
+	  factory.setServiceBean(helloWorld);
+	  //create WebService instance
+	  factory.create();
+	}
+	 
+	public static void main(String[] args) throws InterruptedException {
+	  //now start the webservice server
+	  new Server();
+	  System.out.println("Server ready...");
+	  Thread.sleep(1000 * 600);
+	  System.out.println("Server exit...");
+	  System.exit(0);
+	}
+	}
